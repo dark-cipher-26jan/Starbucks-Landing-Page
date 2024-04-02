177e26d59b5b754bdcfe02d81cc1c7cab59f2b33
Anuz Yadav <158555259+dark-cipher-26jan@users.noreply.github.com>
2024-04-02 11:10:13 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function imgSlider(anything){
+  document.querySelector('.starbucks').src = anything;
+}
+
+
+ /*
+* Copyright (c) 2024 Anuz
+*
+* Author: Anuz Yadav
+* Email: darkcipher26jan2024@gmail.com
+* GitHub: https://github.com/dark-cipher-26jan
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/ 
